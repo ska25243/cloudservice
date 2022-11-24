6c869cb369e22f55ef713f27d3283d5e1b10a41f
DESKTOP-PT3CVHU\Desktop <ska2524@naver.com>
2022-11-24 22:06:32 +0900

    feat : token , refresh token 추가

---
+++ gateway-service/src/main/java/com/wildmeet/gatewayservice/fallback/FallbackController.java
@@ -0,0 +1,31 @@
+package com.wildmeet.gatewayservice.fallback;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class FallbackController {
+
+    @RequestMapping(value = "/fallback")
+    @ResponseStatus
+    public Mono<Map<String, Object>> fallback(ServerWebExchange exchange, Throwable throwable) {
+        Map<String, Object> result = new HashMap<>(8);
+        ServerHttpRequest request = exchange.getRequest();
+        result.put("path", request.getPath().pathWithinApplication().value());
+        result.put("method", request.getMethodValue());
+        if (null != throwable.getCause()) {
+            result.put("message", throwable.getCause().getMessage());
+        } else {
+            result.put("message", throwable.getMessage());
+        }
+        return Mono.just(result);
+    }
+
+}
--- gateway-service/src/main/java/com/wildmeet/gatewayservice/jwt/JwtUtil.java
+++ gateway-service/src/main/java/com/wildmeet/gatewayservice/jwt/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.wildmeet.gatewayservice.jwt;
 
+import com.wildmeet.gatewayservice.jwt.domain.TokenInfo;
 import io.jsonwebtoken.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,13 +27,42 @@ public class JwtUtil {
         return null;
     }
 
-    public String generateToken(String id) {
+    public TokenInfo generateToken(String id) {
         Claims claims = Jwts.claims().setSubject(id);
+//        long nowMillis = System.currentTimeMillis();
+//        long expMillis = nowMillis + tokenValidity;
+//        Date exp = new Date(expMillis);
+//        return Jwts.builder().setClaims(claims).setIssuedAt(new Date(nowMillis)).setExpiration(exp)
+//                .signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+
+//        String authorities = authentication.getAuthorities().stream()
+//                .map(GrantedAuthority::getAuthority)
+//                .collect(Collectors.joining(","));
+
+        String name = "sys";
+
         long nowMillis = System.currentTimeMillis();
         long expMillis = nowMillis + tokenValidity;
+        // Access Token 생성
         Date exp = new Date(expMillis);
-        return Jwts.builder().setClaims(claims).setIssuedAt(new Date(nowMillis)).setExpiration(exp)
-                .signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+        String accessToken = Jwts.builder()
+                .setSubject(name)
+                .claim("auth", claims)
+                .setExpiration(exp)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+
+        // Refresh Token 생성
+        String refreshToken = Jwts.builder()
+                .setExpiration(exp)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+
+        return TokenInfo.builder()
+                .grantType("Bearer")
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
     }
 
     public void validateToken(final String token) throws JwtException {
